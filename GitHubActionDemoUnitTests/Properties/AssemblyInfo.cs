18744fc1f8eafde0ae13d5c483884e3f6c909576
Duran Hsieh <dog0416@gmail.com>
2022-03-16 21:40:27 +0800

    加入專案檔案。

---
+++ GitHubActionDemoUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHubActionDemoUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitHubActionDemoUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51a8365c-5add-4be7-8bd4-8eefdb0ad178")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GitHubActionDemoUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GitHubActionDemoUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(1, 1, 2)]
+        [DataRow(12, 30, 42)]
+        [DataRow(14, 1, 15)]
+        public void Test_Add(int a, int b, int expected)
+        {
+            var actual = a + b;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
